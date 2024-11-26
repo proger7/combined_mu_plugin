1175f73b6b972b337a22124df78b00f875e92ee6
proger7 <quantumwise555@gmail.com>
2024-11-26 14:49:24 +0200

    updated version of combined mu plugin

---
+++ table-client-all/mob/offers-data.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    "offers" => [
+        "c-date" => array(
+            "brandName" => "C-Date",
+            "bulletPoints" => "
+                Sample Text
+                Casual Dating Site
+                Find & meet singles tonight!
+                Free registration
+                No pressure & fixed rules
+                Profiles, users & photos verified
+            ",
+            "price" => "$50/m",
+            "linkID" => "12345",
+        ),
+        "ashley-madison" => array(
+            "brandName" => "Ashley Madison",
+            "bulletPoints" => "
+                Another Sample Text
+                Discover amazing singles
+                Fast sign-up
+                Great reviews
+                Trusted service
+            ",
+            "price" => "$30/m",
+            "linkID" => "67890",
+        ),
+    ],
+    "models" => [
+        "anna" => array(
+            "Name" => "Anna",
+            "Age" => "26",
+            "Location" => "Mexico",
+            "Occupation" => "Singer",
+            "Interests" => "Making Tacos",
+            "Tag" => "latina",
+        ),
+        "irina" => array(
+            "Name" => "Irina",
+            "Age" => "30",
+            "Location" => "Odesa, Ukraine",
+            "Occupation" => "Nurse",
+            "Interests" => "Tennis, and helping people",
+            "Tag" => "ua",
+        ),
+    ],
+    "brands" => [
+        "ashley-madison" => array("brandName" => "Ashley Madison"),
+        "c-date" => array("brandName" => "C Date"),
+    ],
+];
