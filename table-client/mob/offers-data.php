4bccb39d7040be23ad5b5c3f32404510c5c7d334
proger7 <quantumwise555@gmail.com>
2024-12-26 15:30:04 +0200

    fix edits in style1 and add array data

--- table-client/mob/offers-data.php
+++ table-client/mob/offers-data.php
@@ -4,29 +4,98 @@ return [
         "c-date" => array(
             "brandName" => "C-Date",
             "bulletPoints" => "
-                Sample Text
-                Casual Dating Site
-                Find & meet singles tonight!
+                Casual dating site
+                Find and meet singles tonight
                 Free registration
-                No pressure & fixed rules
-                Profiles, users & photos verified
+                No pressure and fixed rules
+                Verified profiles, users, and photos
             ",
             "price" => "$50/m",
             "linkID" => "12345",
-            "siteLabel" => "Site of the Day",
         ),
         "ashley-madison" => array(
             "brandName" => "Ashley Madison",
             "bulletPoints" => "
-                Another Sample Text
                 Discover amazing singles
                 Fast sign-up
                 Great reviews
                 Trusted service
+                Discreet and secure
             ",
             "price" => "$30/m",
             "linkID" => "67890",
-            "siteLabel" => "Featured Site",
+        ),
+        "adultfriendfinder" => array(
+            "brandName" => "AdultFriendFinder",
+            "bulletPoints" => "
+                Connect with like-minded individuals
+                Large user base
+                Easy to use
+                Various features for interaction
+                Free to join
+            ",
+            "price" => "$40/m",
+            "linkID" => "78901",
+        ),
+        "friend-finder" => array(
+            "brandName" => "FriendFinder",
+            "bulletPoints" => "
+                Meet new friends
+                Chat and connect
+                Safe and secure environment
+                Free registration
+                Diverse community
+            ",
+            "price" => "$25/m",
+            "linkID" => "23456",
+        ),
+        "benaughty" => array(
+            "brandName" => "BeNaughty",
+            "bulletPoints" => "
+                Flirt and chat with singles
+                Fun and casual atmosphere
+                Easy to navigate
+                Free to join
+                Verified profiles
+            ",
+            "price" => "$35/m",
+            "linkID" => "34567",
+        ),
+        "jerkmate" => array(
+            "brandName" => "Jerkmate",
+            "bulletPoints" => "
+                Live adult entertainment
+                Connect with performers
+                Interactive experience
+                Free to join
+                Various categories available
+            ",
+            "price" => "$45/m",
+            "linkID" => "45678",
+        ),
+        "victoriamilan" => array(
+            "brandName" => "VictoriaMilan",
+            "bulletPoints" => "
+                Discreet dating for married individuals
+                Safe and secure platform
+                Large user base
+                Free registration
+                Verified profiles
+            ",
+            "price" => "$60/m",
+            "linkID" => "56789",
+        ),
+        "searching-for-singles" => array(
+            "brandName" => "SearchingForSingles",
+            "bulletPoints" => "
+                Find your perfect match
+                Easy to use interface
+                Free to join
+                Various features for interaction
+                Safe and secure environment
+            ",
+            "price" => "$30/m",
+            "linkID" => "67890",
         ),
     ],
     "models" => [
