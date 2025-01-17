c8f531b7e226d90a592510e29ca6d7c567ccbb60
proger7 <quantumwise555@gmail.com>
2025-01-17 10:08:26 +0200

    rename classes for casino2 newhorrizon site

--- wp-content/mu-plugins/casino-offer-tables/includes/shortcode.php
+++ wp-content/mu-plugins/casino-offer-tables/includes/shortcode.php
@@ -85,52 +85,52 @@ function casino_offer_tables_shortcode($atts) {
 
     } elseif ($style === 'casino2') {
 
-                $output .= '<div class="toplist-poker-compact__offers">';
+                $output .= '<div class="newhorrizon_toplist-poker-compact__offers">';
 
                 foreach ($filteredOffers as $arr_key => $offer) {
                     $offerLinkURL = site_url() . "/out/offer.php?id=" . esc_attr($offer['linkID']) . "&o=" . urlencode($arr_key) . "&t=dating";
 
-                    $output .= '<div class="toplist-poker-compact__offer" data-id="' . esc_attr($offer['linkID']) . '" data-offer-name="' . esc_attr($offer['brandName']) . '">';
-                    $output .= '<div class="toplist-poker-compact__offer-wrapper">';
-                    $output .= '<div class="toplist-poker-compact__offer-inner">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer" data-id="' . esc_attr($offer['linkID']) . '" data-offer-name="' . esc_attr($offer['brandName']) . '">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-wrapper">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-inner">';
 
-                    $output .= '<div class="toplist-poker-compact__offer-logo-wrapper">';
-                    $output .= '<div class="toplist-poker-compact__offer-label"></div>';
-                    $output .= '<a href="' . esc_url($offerLinkURL) . '" class="toplist-poker-compact__offer-logo" title="' . esc_attr($offer['brandName']) . '" target="_blank" rel="nofollow sponsored">';
-                    $output .= '<div class="toplist-poker-compact__offer-logo-inner">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-logo-wrapper">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-label"></div>';
+                    $output .= '<a href="' . esc_url($offerLinkURL) . '" class="newhorrizon_toplist-poker-compact__offer-logo" title="' . esc_attr($offer['brandName']) . '" target="_blank" rel="nofollow sponsored">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-logo-inner">';
                     $output .= '<img alt="Logo" src="' . esc_url($offer['logo']) . '">';
                     $output .= '</div>';
                     $output .= '</a>';
                     $output .= '</div>';
 
-                    $output .= '<div class="toplist-poker-compact__offer-head">';
-                    $output .= '<div class="toplist-poker-compact__offer-brand-name">' . esc_html($offer['brandName']) . '</div>';
-                    $output .= '<a href="' . esc_url($offerLinkURL) . '" class="toplist-poker-compact__offer-title">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-head">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-brand-name">' . esc_html($offer['brandName']) . '</div>';
+                    $output .= '<a href="' . esc_url($offerLinkURL) . '" class="newhorrizon_toplist-poker-compact__offer-title">';
                     $output .= '<p>' . esc_html($offer['bonus']) . '</p>';
                     $output .= '</a>';
                     $output .= '</div>';
 
-                    $output .= '<div class="toplist-poker-compact__offer-body snipcss0-4-4-14">';
-                    $output .= '<div class="toplist-poker-compact__offer-key-features snipcss0-5-14-15">';
-                    $output .= '<ul class="snipcss0-6-15-16">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-body">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-key-features">';
+                    $output .= '<ul>';
                     $bulletPoints = preg_split('/\r\n|\r|\n/', trim($offer['bulletPoints']));
                     foreach ($bulletPoints as $feature) {
-                        $output .= '<li class="principales-list-item snipcss0-7-16-17"><span class="list-check-icon snipcss0-8-17-18"></span>' . esc_html($feature) . '</li>';
+                        $output .= '<li class="newhorrizon_principales-list-item"><span class="newhorrizon_list-check-icon"></span>' . esc_html($feature) . '</li>';
                     }
                     $output .= '</ul>';
                     $output .= '</div>';
                     $output .= '</div>';
 
-                    $output .= '<div class="toplist-poker-compact__offer-extra snipcss0-4-4-23">';
-                    $output .= '<div class="toplist-poker-compact__offer-rating-review-wrapper snipcss0-5-23-24">';
-                    $output .= '<div class="toplist-poker-compact__offer-rating-wrapper snipcss0-6-24-25">';
-                    $output .= '<div class="toplist-poker-compact__offer-rating-star snipcss0-7-25-26"></div>';
-                    $output .= '<div class="toplist-poker-compact__offer-rating snipcss0-7-25-27">' . esc_html($offer['rating']) . '/<span class="snipcss0-8-27-28">10</span></div>';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-extra">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-rating-review-wrapper">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-rating-wrapper">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-rating-star"></div>';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-rating">' . esc_html($offer['rating']) . '/<span>10</span></div>';
                     $output .= '</div>';
                     $output .= '</div>';
-                    $output .= '<div class="toplist-poker-compact__offer-rakeback snipcss0-5-23-29">No Rakeback</div>';
-                    $output .= '<a href="' . esc_url($offerLinkURL) . '" class="toplist-poker-compact__offer-cta-btn snipcss0-5-23-30" target="_blank" rel="nofollow sponsored">';
-                    $output .= '<div class="toplist-poker-compact__offer-cta-btn-icon snipcss0-6-30-31"></div>Sign Up';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-rakeback">No Rakeback</div>';
+                    $output .= '<a href="' . esc_url($offerLinkURL) . '" class="newhorrizon_toplist-poker-compact__offer-cta-btn" target="_blank" rel="nofollow sponsored">';
+                    $output .= '<div class="newhorrizon_toplist-poker-compact__offer-cta-btn-icon"></div>Sign Up';
                     $output .= '</a>';
                     $output .= '</div>';
 
@@ -140,6 +140,7 @@ function casino_offer_tables_shortcode($atts) {
                 }
 
                 $output .= '</div>';
+
 
     }
 
