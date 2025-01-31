9eb624ebf27937ecfd9e46ab70f413515e0fd2d3
proger7 <quantumwise555@gmail.com>
2025-01-31 10:55:29 +0200

    add missing files to MU folder plugin

---
+++ wp-content/mu-plugins/brand-models-shortcode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Custom Shortcodes Task 2 
+ * Description: Auto-Add Shortcodes
+ * Version: 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+include( ABSPATH . 'table-client-brand-models/mob/functions.php' );
