f71d32cdfff8fdab8b415e9a8fcd99fab6bf7697
proger7 <quantumwise555@gmail.com>
2025-02-10 17:36:01 +0200

    Rename classes for widget models and offers

--- table-client/mob/combined-widget/filter.js
+++ table-client/mob/combined-widget/filter.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const filters = document.querySelectorAll(".theamailorderbride_radio");
+    const filters = document.querySelectorAll(".widget_models_theamailorderbride_radio");
 
     filters.forEach(filter => {
         filter.addEventListener("click", function() {
             let selectedTag = this.getAttribute("data-tag");
 
-            filters.forEach(f => f.classList.remove("theamailorderbride_active"));
-            this.classList.add("theamailorderbride_active");
+            filters.forEach(f => f.classList.remove("widget_models_theamailorderbride_active"));
+            this.classList.add("widget_models_theamailorderbride_active");
 
             let formData = new FormData();
             formData.append("action", "filter_models");
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     if (filters.length > 0) {
-        filters[0].classList.add("theamailorderbride_active");
+        filters[0].classList.add("widget_models_theamailorderbride_active");
         filters[0].click();
     }
 });
